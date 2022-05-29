0b1ec51c281cb022d1cfd1d762e6d13401e028d8
parnlees <yanisa0439@gmail.com>
2022-05-29 16:42:20 +0700

    4th

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
 import React from "react";
 import logo from "../image/logo.png";
-import { Button } from "reactstrap";
+import { button } from "bootstrap";
 
 function Header() {
   return (
     <div className="d-flex justify-content-between container">
       <div className="d-flex align-items-center">
-        <img style={{ width: "35px", height: "35px" }} src={logo} alt="logo" />
-        <h6 className="align-self-center mb-0">
+        <img style={{ width: "50px", height: "50px" }} src={logo} alt="logo" />
+        <h6 className="align-self-center mb-0 text-light">
           <b>Welcome Home</b>
         </h6>
       </div>
       <div className="d-flex align-items-center">
-        <Button className="mx-3">Login</Button>
-        <Button>Contact Us</Button>
+        <button className="mx-3 btn btn-outline-info">Login</button>
+        <button className="mx-3 btn btn-outline-info">Contact Us</button>
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,16 +7,15 @@ import {
   CardSubtitle,
   CardBody,
   CardTitle,
-  CardGroup,
 } from "reactstrap";
 import { button } from "bootstrap";
 import Frenchfries from "../image/Frenchfries.jpg";
-import Hamburger from "../image/Hamburger.jpg";
+import Hamburger2 from "../image/Hamburger2.jpg";
 
 function Home() {
   return (
-    <div className="body">
-      <CardGroup>
+    <div className="body d-flex align-items-center justify-content-center">
+      <div className="d-flex align-content-around justify-content-evenly flex-wrap">
         <Card style={{ width: "300px", height: "350px" }}>
           <div className="d-flex align-items-center justify-content-center">
             <CardImg
@@ -50,7 +49,36 @@ function Home() {
           <div className="d-flex align-items-center justify-content-center">
             <CardImg
               alt="Hamburger"
-              src={Hamburger}
+              src={Hamburger2}
+              style={{ width: "150px", height: "100px" }}
+            />
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <CardBody>
+              <CardTitle tag="h5">Hamburger</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                Main Menu !
+              </CardSubtitle>
+              <CardText>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum
+                harum consequuntur, ipsa illum ut a? Suscipit illum enim cumque
+                deserunt?
+              </CardText>
+              <button
+                type="button"
+                className="d-flex align-items-center justify-content-center btn btn-outline-dark btn-small"
+              >
+                Bucket
+              </button>
+            </CardBody>
+          </div>
+        </Card>
+
+        <Card style={{ width: "300px", height: "350px" }}>
+          <div className="d-flex align-items-center justify-content-center">
+            <CardImg
+              alt="Hamburger"
+              src={Hamburger2}
               style={{ width: "150px", height: "100px" }}
             />
           </div>
@@ -74,7 +102,96 @@ function Home() {
             </CardBody>
           </div>
         </Card>
-      </CardGroup>
+      </div>
+
+      <div className="d-flex align-content-around justify-content-evenly flex-wrap">
+        <Card style={{ width: "300px", height: "350px" }}>
+          <div className="d-flex align-content-between justify-content-center">
+            <CardImg
+              alt="Frenchfries"
+              src={Frenchfries}
+              style={{ width: "150px", height: "100px" }}
+            />
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <CardBody>
+              <CardTitle tag="h5"> Frenchfries</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                hot dishes!
+              </CardSubtitle>
+              <CardText>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum
+                harum consequuntur, ipsa illum ut a? Suscipit illum enim cumque
+                deserunt?
+              </CardText>
+              <button
+                type="button"
+                className="d-flex align-items-center justify-content-center btn btn-outline-dark btn-small"
+              >
+                Bucket
+              </button>
+            </CardBody>
+          </div>
+        </Card>
+
+        <Card style={{ width: "300px", height: "350px" }}>
+          <div className="d-flex align-items-center justify-content-center">
+            <CardImg
+              alt="Hamburger"
+              src={Hamburger2}
+              style={{ width: "150px", height: "100px" }}
+            />
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <CardBody>
+              <CardTitle tag="h5"> Hamburger</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                Main Menu !
+              </CardSubtitle>
+              <CardText>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum
+                harum consequuntur, ipsa illum ut a? Suscipit illum enim cumque
+                deserunt?
+              </CardText>
+              <button
+                type="button"
+                className="d-flex align-items-center justify-content-center btn btn-outline-dark btn-small"
+              >
+                Bucket
+              </button>
+            </CardBody>
+          </div>
+        </Card>
+
+        <Card style={{ width: "300px", height: "350px" }}>
+          <div className="d-flex align-items-center justify-content-center">
+            <CardImg
+              alt="Hamburger"
+              src={Hamburger2}
+              style={{ width: "150px", height: "100px" }}
+            />
+          </div>
+          <div className="d-flex align-items-center justify-content-center">
+            <CardBody>
+              <CardTitle tag="h5"> Hamburger</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                Main Menu !
+              </CardSubtitle>
+              <CardText>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum
+                harum consequuntur, ipsa illum ut a? Suscipit illum enim cumque
+                deserunt?
+              </CardText>
+              <button
+                type="button"
+                className="d-flex align-items-center justify-content-center btn btn-outline-dark btn-small"
+              >
+                Bucket
+              </button>
+            </CardBody>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
